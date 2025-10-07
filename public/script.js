10a48f4dc863214bf9f1807f17434b1040887f19
{aron} <aronkovacs138@gmail.com>
2025-10-07 09:56:46 +0200

    a

--- public/script.js
+++ public/script.js
@@ -345,7 +345,7 @@ function CARD_BETOLT(adatok){
     s += "</div>";
 
 
-    if ($("#nev1").val() != "") $("#keresett_kifejezes").html(`Találatok a(z) <b>"${$("#nev1").val()}"</b> kifejezésre (<span id="débé"></span>)`);
+    if ($("#nev1").val() != "") $("#keresett_kifejezes").html(`Találatok a(z) <b>"${$("#nev1").val()}"</b> kifejezésre`);
     else {$("#keresett_kifejezes").html("")};
 
     
