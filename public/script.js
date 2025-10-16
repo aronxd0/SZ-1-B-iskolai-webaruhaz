48a5493eef111b10e42700d0f4bdfb5d8024e61a
szalokyaron <szaloky.aron@csany-zeg.hu>
2025-10-16 15:35:03 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -218,10 +218,14 @@ async function KosarTetelDB() {
     } catch (err) { üzen(err, "danger"); }
 }
 async function KosarPLUSZ(id){
-    await ajax_post(`kosar_add?ID_TERMEK=${id.id}`, 1)
-    var db = await ajax_post("tetelek?ID_TERMEK="+id.id, 1)
-    document.getElementById(`${id.id}2`).innerHTML = db.rows[0].MENNYISEG;
-    document.getElementById(`${id.id}3`).innerHTML = ` <h5><b >${parseInt(db.rows[0].MENNYISEG) * parseInt(db.rows[0].AR)} Ft</b><h5>` ;// mindegyiknek igyanaz az idj ? nem jó majd othonm nekiállok .😓
+    var PluszVAGYminusz = id.id.substring(id.id.length - 1, id.id.length) == 9? 0 : -1  ;// ha nem 9 akkor plusz
+
+    var idk = id.id.substring(0, id.id.length - 1);
+    
+    await ajax_post(`kosar_add?ID_TERMEK=${idk}`, 1);
+    var db = await ajax_post("tetelek?ID_TERMEK="+idk, 1);
+    document.getElementById(`${idk}2`).innerHTML = db.rows[0].MENNYISEG;
+    document.getElementById(`${idk}3`).innerHTML = `<h5><b >${parseInt(db.rows[0].MENNYISEG) * parseInt(db.rows[0].AR)} Ft</b><h5>` ;// mindegyiknek igyanaz az idj ? nem jó majd othonm nekiállok .😓
     KosarTetelDB();
 
     };
@@ -1228,7 +1232,7 @@ $(document).ready(function() {
                     ts += `<div class="col-4" style="height: 100px" >  <img src="${element.FOTOLINK}" class="img-fluid" alt="Card image" style="height:100px"> </div>`;
                     ts += `<div class="col-4"> <h4>${element.NEV}</h4> </div>`;
                     ts += `<div class="col-2 text-center text-white m-auto" id="${element.ID_TERMEK}3"><h5><b > ${element.AR * element.MENNYISEG} Ft</b><h5> </div>`;
-                    ts += `<div class="col-2 m-auto"> <button type="button" class="btn btn-danger"><i class="bi bi-dash-circle"></i></button><span class="p-2" id="${element.ID_TERMEK}2">${element.MENNYISEG}</span> db   <button type="button" id="${element.ID_TERMEK}" onclick="KosarPLUSZ(this)" class="btn btn-success"><i class="bi bi-plus-circle"></i></button> <button type="button" class="btn btn-warning"><i class="bi bi-x-circle-fill"></i></button></div> `;
+                    ts += `<div class="col-2 m-auto"> <button type="button" class="btn btn-danger"><i class="bi bi-dash-circle" id="${element.ID_TERMEK}9" onclick="KosarPLUSZ(this)"></i></button><span class="p-2" id="${element.ID_TERMEK}2">${element.MENNYISEG}</span> db   <button type="button" id="${element.ID_TERMEK}1" onclick="KosarPLUSZ(this)" class="btn btn-success"><i class="bi bi-plus-circle"></i></button> <button type="button" class="btn btn-warning"><i class="bi bi-x-circle-fill"></i></button></div> `;
         
                     ts += "</div>"
                 }   
