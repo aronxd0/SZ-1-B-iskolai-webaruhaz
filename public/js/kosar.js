c854d38ef498f6c47d34d4bf20e804a93e782a9c
{aron} <aronkovacs138@gmail.com>
2025-10-17 14:03:33 +0200

    Update kosar.js

--- public/js/kosar.js
+++ public/js/kosar.js
@@ -13,87 +13,62 @@ $("#cart_button").click(async function () {
 
     try {
         await ajax_post("tetelek", 1).then(tetelek => {
-            for (const element of tetelek.rows) {
-                
-                let pez = element.AR * element.MENNYISEG;
 
+             
+
+            if (tetelek.rows.length > 0) {
+                for (const element of tetelek.rows) {
+                    
+                    let pez = element.AR * element.MENNYISEG;
+
+                    cnt += `
+                        <div class="d-flex flex-column flex-xxl-row feka shadow-lg rounded-4 mt-3 p-3 p-xxl-none" id="${element.ID_TERMEK}NAGY">
+                            <div class="col-1"></div>
+                            <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center">
+                                <img src="${element.FOTOLINK}" class="img img-fluid" style="width:30%;" alt="kep">
+                            </div>
+                            <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center p-3">
+                                <p>${element.NEV}</p>
+                            </div>
+                            <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center">
+                                <button type="button" class="btn btn-lg btn-secondary bi bi-dash-lg bal-gomb" aria-label="minusz" onclick="KosarPLUSZ(this)" id="${element.ID_TERMEK}9"></button>
+                                <input type="number" class="form-control-lg w-50" min="1" onchange="KosarPLUSZ(this)" value="${element.MENNYISEG}" id="${element.ID_TERMEK}2">
+                                <button type="button" class="btn btn-lg btn-secondary bi bi-plus-lg jobb-gomb" aria-label="plusz" onclick="KosarPLUSZ(this)" id="${element.ID_TERMEK}1"></button>
+                            </div>
+                            <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center p-3" id="${element.ID_TERMEK}3">
+                                <h4 class="anton-regular text-success">${pez.toLocaleString()} Ft</h4>
+                            </div>
+
+                            <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center">
+                                <button type="button" id="${element.ID_TERMEK}" onclick="KosarItemDelete(this)" class="btn btn-danger" aria-label="teteltorol"><i class="bi bi-x-lg"></i></button>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>        
+                    
+            
+                    `;
+       
+                }
+            }
+            else {
                 cnt += `
-                    <div class="d-flex flex-column flex-xxl-row feka shadow-lg rounded-4 mt-3 p-3 p-xxl-none" id="${element.ID_TERMEK}NAGY">
-                        <div class="col-1"></div>
-                        <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center">
-                            <img src="${element.FOTOLINK}" class="img img-fluid" style="width:30%;" alt="kep">
-                        </div>
-                        <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center p-3">
-                            <p>${element.NEV}</p>
-                        </div>
-                        <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center">
-                            <button type="button" class="btn btn-lg btn-secondary bi bi-dash-lg bal-gomb" aria-label="minusz" onclick="KosarPLUSZ(this)" id="${element.ID_TERMEK}9"></button>
-                            <input type="number" class="form-control-lg w-50" min="1" onchange="KosarPLUSZ(this)" value="${element.MENNYISEG}" id="${element.ID_TERMEK}2">
-                            <button type="button" class="btn btn-lg btn-secondary bi bi-plus-lg jobb-gomb" aria-label="plusz" onclick="KosarPLUSZ(this)" id="${element.ID_TERMEK}1"></button>
-                        </div>
-                        <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center p-3" id="${element.ID_TERMEK}3">
-                            <h4 class="anton-regular text-success">${pez.toLocaleString()} Ft</h4>
-                        </div>
+                    <div class="col-12">
+                        <div class="text-center p-2" id="kosarmenutitle"><h5>A Kosarad ures</h5></div>
+                    </div>
+            
+            `;
+            }
+                   
+            });
 
-                        <div class="col-12 col-xxl-2 d-flex align-self-center justify-content-center">
-                            <button type="button" id="${element.ID_TERMEK}" onclick="KosarItemDelete(this)" class="btn btn-danger" aria-label="teteltorol"><i class="bi bi-x-lg"></i></button>
-                        </div>
-                        <div class="col-1"></div>
-                    </div>        
-                
-        
-                
-                `;
+        } catch (err) { console.log("hiba:", err); }
 
 
 
+       
 
-
-                ts += `<div class="col-12 d-flex flex-column flex-lg-row" id="${element.ID_TERMEK}NAGY">`;
-
-                ts += `<img src="${element.FOTOLINK}" class="img-fluid" style="width:50%;height:50%;" alt="kep"> `;
-                ts += `<h4>${element.NEV}</h4>`;
-
-                ts += `
-                
-                <div >
-                            <div class="input-group-button oclickable" onclick="KosarPLUSZ(this)" id="${element.ID_TERMEK}9">
-                                <span class="input-number-decrement">-</span>
-                            </div>
-                            <input class="input-number" type="number" onchange="KosarPLUSZ(this)" value="${element.MENNYISEG}" id="${element.ID_TERMEK}2" min="1" >
-                            <div class="input-group-button oclickable" onclick="KosarPLUSZ(this)" id="${element.ID_TERMEK}1">
-                                <span class="input-number-increment">+</span>
-                            </div>
-                          
-                            </div>
-                          (db)
-                     
-                `
-
-                //ts += `<div class="col-2 m-auto"> <button type="button" class="btn btn-danger"><i class="bi bi-dash-circle" id="${element.ID_TERMEK}9" onclick="KosarPLUSZ(this)"></i></button><span class="p-2" id="${element.ID_TERMEK}2">${element.MENNYISEG}</span> db   <button type="button" id="${element.ID_TERMEK}1" onclick="KosarPLUSZ(this)" class="btn btn-success"><i class="bi bi-plus-circle"></i></button> </div> `;
-                ts += `<div id="${element.ID_TERMEK}3"><h5><b > ${element.AR * element.MENNYISEG} Ft</b><h5>`;               
-                ts += `<div ><button type="button" id="${element.ID_TERMEK}" onclick="KosarItemDelete(this)" class="btn btn-warning"><i class="bi bi-x-circle-fill"></i></button> `;
-                ts += "</div>" 
-            }   
-        });
-
-    } catch (err) { console.log("hiba:", err); }
-
-
-
-    var kd = `
-        <div class="col-12">
-            <div class="text-center p-2 bg-success" id="kosarmenutitle"><h1>ha a kosar ures akkor "kosar ures" ha nem akkor kosar tartalma</h1> </div>
-            <div class="feka p-2" id="kosar_tetelek">
-                ide jonnek a tetelek  | Nagyon sok backend ÁDI készülj, alvásnak vége munka lesz
-            </div>
-        </div>
-    
-    `;
-
-    console.log(ts +" ez a tr")
-    $("#content_hely").html(cnt);
-    $("#pagi").html("");
+        $("#content_hely").html(cnt);
+        $("#pagi").html("");
 });
 
 
@@ -133,7 +108,10 @@ async function KosarTetelDB() {
         var db = 0;
         for (const element of kosarteteldb.rows) {
             console.log(typeof element.kdb);
-            if (element.kdb == undefined) { db = 0;} 
+            if (element.kdb == undefined) { 
+                db = 0;
+
+            } 
             else { db = parseInt(element.kdb); }
             $("#kosar_content_count").html(`${db}`);
         }
