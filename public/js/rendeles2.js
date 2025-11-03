82af2b010e3a66c6ae5ea73e8c22c8df17676738
szalokyaron <szaloky.aron@csany-zeg.hu>
2025-11-03 08:50:56 +0100

    Rendeladsas

---
+++ public/js/rendeles2.js
@@ -0,0 +1,10 @@
+
+$("#rend_button").click(async function () {
+   
+    console.log("rendeles2.js rend_button click");
+    $("#keresett_kifejezes").html("");
+    $("#débé").html("");
+    $("#nev1").val("");
+    $("#pagi").html("");
+    $("#content_hely").html("ido jön a rendeles");
+});
