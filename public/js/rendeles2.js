fe87bec216d138002f9e5c6f6eaef34897cff39b
szalokyaron <szaloky.aron@csany-zeg.hu>
2025-11-03 08:57:07 +0100

    wsad

--- public/js/rendeles2.js
+++ public/js/rendeles2.js
@@ -1,10 +1,21 @@
 
 $("#rend_button").click(async function () {
    
+    let ureskosar = `
+                <div class="col-12">
+                    <div class="text-center p-2" id="kosarmenutitle"><h5>A boltunkban még nem vásásroltál.</h5></div>
+                </div>`;
+
+
+
     console.log("rendeles2.js rend_button click");
     $("#keresett_kifejezes").html("");
     $("#débé").html("");
     $("#nev1").val("");
     $("#pagi").html("");
-    $("#content_hely").html("ido jön a rendeles");
+   
+
+    $("#content_hely").fadeOut(300, function() {
+        $("#content_hely").html(ureskosar).fadeIn(300);
+    });
 });
