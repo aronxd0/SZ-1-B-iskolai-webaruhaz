1266a108bde7c6a6cff47773cea0c4193acaa497
szalokyaron <szaloky.aron@csany-zeg.hu>
2025-11-07 13:40:57 +0100

    Update rendeles2.js

--- public/js/rendeles2.js
+++ public/js/rendeles2.js
@@ -1,11 +1,8 @@
 
 $("#rend_button").click(async function () {
     
-    var s ="";
-
-    
+    var s ="";  
     var itemek = await ajax_post("rendelesek",1)
-    console.log(`ITEMEK + ${JSON.stringify(itemek)}`);
     
     if(itemek.maxcount !=  0){
         for(const elemek of itemek.rows ){
@@ -69,7 +66,7 @@ $("#rend_button").click(async function () {
     else{
         s = `
         <div class="col-12">
-            <div class="text-center p-2" id="kosarmenutitle"><h5>A boltunkban még nem vásásroltál.</h5></div>
+            <div class="text-center p-2"><h5>A boltunkban még nem vásásroltál.</h5></div>
         </div>`;
     }
 
