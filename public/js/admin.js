18553c74cb60e8370ce4700ca3f41fe400d38f7d
{aron} <aronkovacs138@gmail.com>
2025-11-14 10:18:09 +0100

    szarok

--- public/js/admin.js
+++ public/js/admin.js
@@ -62,13 +62,36 @@ function SQLinput() {
     $("#home_button").closest(".gombdiv").removeClass("aktiv");
     $("#cart_button").closest(".gombdiv").removeClass("aktiv");
 
+    $("#welcome_section").fadeOut(300);
+
     $("#content_hely").fadeOut(300, function() {
         $("#content_hely").html(`
         <div class="row">
             <div class="col-12 text-center p-2 mt-3">
-                <span class="text-xl">sql input</span>
+                <span class="text-xl">SQL Input</span>
             </div>
         </div>
+
+        <div class="row mt-3">
+            <div class="col-12 p-5">
+                <textarea id="sql_input_area" 
+                class="
+                resize-none 
+                form-control 
+                shadow-xl 
+                bg-zinc-50 
+                text-slate-900 
+                dark:bg-slate-900 
+                dark:text-zinc-200 
+                p-3 
+                rounded-4 
+                placeholder-gray-400 
+                dark:placeholder-gray-400 
+                " rows="10" placeholder="Ide írd be az SQL lekérdezést..." style="border:none;"></textarea>
+            </div>
+        </div>
+
+
         `).fadeIn(300);
         $("#pagi").html("");
     })
