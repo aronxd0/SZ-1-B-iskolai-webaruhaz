3187452250fdacb1030e3c2d78313fd46000e35b
szalokyadam <szaloky.adam@csany-zeg.hu>
2025-12-05 13:07:05 +0100

    KEPEK

--- web13-restapi.js
+++ web13-restapi.js
@@ -90,7 +90,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 12 * 1024 * 1024 },
+  limits: { fileSize: 25 * 1024 * 1024 }, //25mb 
   fileFilter: fileFilter
 });
 // === MIME TÍPUS ELLENŐRZÉS ===
