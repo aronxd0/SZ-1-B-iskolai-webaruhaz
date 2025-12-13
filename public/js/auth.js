78baaeb5367e3ebade36cb38800c921c70a72b20
{aron} <aronkovacs138@gmail.com>
2025-12-13 17:44:00 +0100

    hat csinaltam tovabb mindenfele szart xd

--- public/js/auth.js
+++ public/js/auth.js
@@ -46,13 +46,15 @@ function BevaneJelentkezve() {
 
     else { 
         rang = "";
-        $("#user").html(`<h5><i class="bi bi-person"></i> ${bejelentkezett_usernev}</h5>`);
-        $("#user-email").html(`${bejelentkezett_useremail}`);
+        $("#user").html(`<b><i class="bi bi-person"></i></b> ${bejelentkezett_usernev}`);
+        $("#user-email").html(`<b><i class="bi bi-envelope"></i></b> ${bejelentkezett_useremail}`);
         $("#vendegszoveg").html("");
         
         $("#udv").html(`<b>Üdvözlünk a Csány webáruházban <span>${bejelentkezett_usernev.split(" ")[1]}</span>!</b>`);
 
-        rang += `<span class="inline-flex items-center rounded-md bg-blue-400/10 px-2 py-1 text-sm font-medium text-blue-400 !border !border-blue-400/30">${csoport}</span>`;
+        if (csoport == "Students") rang += `<span class="inline-flex items-center rounded-md bg-blue-400/10 px-2 py-1 text-sm font-medium text-blue-400 !border !border-blue-400/30">${csoport}</span>`;
+        else if (csoport == "Teachers") rang += `<span class="inline-flex items-center rounded-md bg-yellow-400/10 px-2 py-1 text-sm font-medium text-yellow-600 !border !border-yellow-600/40">${csoport}</span>`;
+
 
         if (admin) {
             rang += `<span class="inline-flex items-center rounded-md bg-red-400/10 px-2 py-1 text-sm font-medium text-red-400 !border !border-red-400/20">Admin</span>`;
