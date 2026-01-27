71d0d7d62a17b4968b450c059de1faac79fd6f45
{aron} <aronkovacs138@gmail.com>
2026-01-27 11:44:12 +0100

    Update email-sender.js

--- email-sender.js
+++ email-sender.js
@@ -3,30 +3,30 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 async function sendEmail(to, subject, html) {
-    if (!process.env.USER || !process.env.APP_PASSWORD) {
+    if (!process.env.EMAIL_USER || !process.env.APP_PASSWORD) {
         throw new Error('Missing EMAIL_FROM or EMAIL_PASSWORD env');
-    }
-
+    }		
+	
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 587,
         secure: false,
         auth: {
-            user: process.env.USER,
+            user: process.env.EMAIL_USER,
             pass: process.env.APP_PASSWORD,
         }
     });
-    const fs = require('fs'); // fájl beolvasásához
+    const fs = require('fs'); // fajl beolvasasahoz
 
     const info = await transporter.sendMail({
-        from: `"Csány webaruhaz" <${process.env.USER}>`,
+        from: `"CsĂˇny webaruhaz" <${process.env.EMAIL_USER}>`,
         to,
         subject,
         html,
         attachments: [{
-            filename: 'logo2.png', // csatolt fájl neve
-            content: fs.readFileSync("public/img/logo2.png"), // fájl beolvasása ami csatolva lesz
-            cid: 'logo2@example.com' // azonosító a HTML-ben való hivatkozáshoz
+            filename: 'logo2.png', // csatolt fajl neve
+            content: fs.readFileSync("public/img/logo2.png"), // fajl beolvasasa ami csatolva lesz
+            cid: 'logo2@example.com' // azonosi­to a HTML-ben valo hivatkozashoz
         }],
     });
 
