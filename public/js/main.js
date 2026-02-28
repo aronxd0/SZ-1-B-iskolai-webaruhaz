44c868a6beba08180f1eb6142869df23de014777
{aron} <aronkovacs138@gmail.com>
2026-02-28 18:03:19 +0100

    ez igy jobb lesz

--- public/js/main.js
+++ public/js/main.js
@@ -151,6 +151,13 @@ function KeresoModal() {
       }, 1000);
 }
 
+function UjTermek() {
+    $("#home_button").closest(".gombdiv").removeClass("aktiv");
+    $("#cart_button").closest(".gombdiv").removeClass("aktiv");
+    TermekSzerkesztoAblak(event,0,"bevitel");
+    $("#save_button").html(`<i class="bi bi-plus-lg"></i>&nbsp;Új termék létrehozása`);
+}
+
 function RangokHTML(rang) {
     let ranghtml = "";
     switch (rang) {
